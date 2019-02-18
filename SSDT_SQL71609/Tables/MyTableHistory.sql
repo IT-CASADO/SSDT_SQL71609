bd77dda39d94b37a35f38d99a5b9a3932b386631
Daniel Casado (EVO) <Daniel.Casado@Evolutionizer.com>
2019-02-18 09:18:01 +0100

    Add code for reproduction of SQL71609 error with DATETIME2

---
+++ SSDT_SQL71609/Tables/MyTableHistory.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[MyTableHistory]
+(
+	[TenantId] UNIQUEIDENTIFIER NOT NULL,
+	[Rn] BIGINT IDENTITY(1,1) NOT NULL,
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[PropA] INT NOT NULL,
+	[PropB] NVARCHAR(100) NOT NULL,
+
+	[ValidFrom] DATETIME2,
+	[ValidTo] DATETIME2,
+);
+GO
+CREATE CLUSTERED COLUMNSTORE INDEX [COLIX_MyTableHistory]
+	ON [dbo].[MyTableHistory];
+GO
+CREATE NONCLUSTERED INDEX [IX_ImpactHistory_ValidFrom_ValidTo_Id]
+	ON [dbo].[MyTableHistory] ([ValidFrom], [ValidTo], [Id]);
+GO
